13fa9f1e9772d42546d6cdb907e92c3b1e49e6c6
william <keck.william@gmail.com>
2015-10-26 17:35:27 -0700

    finished up the rest

--- js/helper.js
+++ js/helper.js
@@ -1,14 +1,27 @@
+$(document).ready(function() {
+
 $(".readmore").click(readMoreStuff);
-$(".readless .hide").click(readLessStuff);
+$(".readless").click(readLessStuff);
+$(".learnmore").click(learnMoreStuff);
 
 function readMoreStuff() {
+	event.preventDefault();
 	$(this).toggle();
 	$(this).next().toggle();
 	$(this).next().next().toggle();
 }
 
-function readlessStuff() {
+function learnMoreStuff() {
+	event.preventDefault();
+	$(this).toggle();
+	$(this).next().toggle();	
+}
+
+function readLessStuff() {
+	event.preventDefault();
 	$(this).toggle();
 	$(this).prev().toggle();
 	$(this).prev().prev().toggle();
 }
+
+});
